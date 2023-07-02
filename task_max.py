7429580a64806149d3f33e41bf0f4e0ee75584c2
Mac <mac@MacBook-Air-Mac.local>
2023-07-03 02:39:48 +0500

    taskmax

--- task_max.py
+++ task_max.py
@@ -9,7 +9,13 @@ def find_max(a,b,c):
     return:
         int
     """
-    return 0
+    max=a
+    if max<b:
+        max=b
+    if max<c:
+        max=c
+    return max
+print(find_max(-1,12,3))
 
 # Example:
 # find_max(1,2,3) -> 3
@@ -25,7 +31,15 @@ def find_max_idx(a,b,c):
     return:
         int
     """
-    return 0
+    ind=0
+    if ind>0:
+        ind +=a
+    if ind>0:
+        ind +=b
+    if ind>0:
+        ind +=c
+    return ind
+print(find_max_idx(-1,2,13))
 
 # Example:
 # find_max_idx(10,2,13) -> 3
